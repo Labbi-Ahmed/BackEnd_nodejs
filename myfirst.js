9a91e234170eefa0831c0ce4ed277aaa2a55b489
Labbi-Ahmed <labbi5689@gmail.com>
2023-03-06 08:10:49 +0600

    delete all

--- myfirst.js
+++
@@ -1,6 +0,0 @@
-var http  = require('http');
-var mymd  = require('./myModule.js');
-http.createServer(function(req,res){
-    res.writeHead(200, {'Content-type' : 'text/html'});
-    res.end('hello world! now you write your  '+mymd.myDateTime());
-}).listen(8080);
